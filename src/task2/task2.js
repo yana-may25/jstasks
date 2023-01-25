605befa16e5f5f0a7d6ca3451495fc9c4b3ada6f
Yana <yana.senko25@gmail.com>
2023-01-25 13:55:21 +0100

    removed isEven function

--- src/task2/task2.js
+++ src/task2/task2.js
@@ -1,18 +1,3 @@
-function isEven(n)
-    {
-        if (n>0){
-        if (n===1) return false
-        else return isEven(n-2);
-        }
-          else
-            if(n===0) return true
-        else if (n<0){
-            if (n===1) return false
-        else return isEven(n+2);
-        }
-        
-    }
-
     function min(n, m)
     {
         if (n===m) return "equal"
