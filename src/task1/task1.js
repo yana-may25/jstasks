f284ddc559e5602a388207616a78e5437449d93d
Yana <yana.senko25@gmail.com>
2023-01-26 10:16:48 +0100

    merged lines

--- src/task1/task1.js
+++ src/task1/task1.js
@@ -1,9 +1,7 @@
 function func(n){
     let result = "";
     for (let i = '#' ; i.length<n; i+='#'){
-         result+=i;
-        result+='\n';
+         result+=i + '\n';
     }
     return result;
 }
-func(6)
