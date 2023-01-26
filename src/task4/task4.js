e9965d47fba17609f3c3fe9058309cec8ea7faee
Yana <yana.senko25@gmail.com>
2023-01-26 13:33:44 +0100

    added countBs

--- src/task4/task4.js
+++ src/task4/task4.js
@@ -1,9 +1,11 @@
-function countChar(str, char){
-    if (typeof(str)=== "string" && typeof(char)=== "string" && char.length===1){
+function countChar(str, char) {
     let count = 0;
-    for (let i=0; i<str.length; i++)
+    for (let i = 0; i < str.length; i++)
         if (str.charAt(i) === char)
             count++
-    return count;}
-    else return "please provide valid input"
+    return count;
+};
+
+function countBs(str) {
+    return countChar(str, "B")
 }
