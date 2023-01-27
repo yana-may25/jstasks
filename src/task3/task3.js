c492d87ea21961fb522b4a73564f2368792ff7d5
Yana <yana.senko25@gmail.com>
2023-01-27 11:38:12 +0100

    hope I learned my lesson now

--- src/task3/task3.js
+++ src/task3/task3.js
@@ -1,5 +1,9 @@
 function isEven(n) {
-    if (n === 1) return false
-    else if (n === 0) return true
-    else return (n > 0 ? isEven(n - 2) : isEven(n + 2))
-}
+    if (n === 1) {
+        return false;
+    } if (n === 0) {
+        return true;
+    } else {
+        return n > 0 ? isEven(n - 2) : isEven(n + 2);
+    }
+};
