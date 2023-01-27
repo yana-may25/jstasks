54402eb17442812f4afa9e7343d5f7a252069745
Yana <yana.senko25@gmail.com>
2023-01-27 11:47:07 +0100

    more visually appealing it definetely is

--- src/task4/task4.js
+++ src/task4/task4.js
@@ -1,11 +1,13 @@
 function countChar(str, char) {
     let count = 0;
-    for (let i = 0; i < str.length; i++)
-        if (str.charAt(i) === char)
-            count++
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
+    }
     return count;
 };
 
 function countBs(str) {
-    return countChar(str, "B")
-}
+    return countChar(str, 'B')
+};
