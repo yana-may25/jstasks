88344ade662e8686765625adb2f7bc3faf7cd52e
Yana <yana.senko25@gmail.com>
2023-02-05 18:12:14 +0100

    ...

--- src/task10/task10.js
+++ src/task10/task10.js
@@ -1,8 +1,15 @@
 obj = JSON.PARSE(ANCESTRY_FILE)
 
 function task10(array) {
-    var result = jsObjects.filter(obj => {
-        return obj. === 6
-    })
-
+    let mothers = [];
+    for (item in array) {
+        let mother = item.mother;
+        console.log(mother);
+        if (array.find(item => item.name === mother)) {
+            mothers.push(1);
+        }
+    }
+    return mothers;
 }
+
+task10(obj)
