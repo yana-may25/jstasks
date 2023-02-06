f597db69317ceb93dae134931bb12cc6bbcf9020
Yana <yana.senko25@gmail.com>
2023-02-06 11:26:07 +0100

    added groupBy abstraction

--- src/task11/task11.js
+++ src/task11/task11.js
@@ -1,22 +1,34 @@
-obj = JSON.parse(ANCESTRY_FILE);
+var obj = JSON.parse(ANCESTRY_FILE);
 
-function lifeExpectancyByCentury(items) {
+function groupBy(items, func) {
     let centuries = [];
     for (let item of items) {
-        let century = Math.ceil(item.died / 100), ageSum = item.died - item.born;
-        let known = centuries.findIndex(c => c.century == century);
+        let people = [];
+        let century = func(item.died);
+        let known = centuries.findIndex(c => c.century === century);
         if (known == -1) {
-            centuries.push({ century, ageSum, count: 1 });
+            people.push(item);
+            centuries.push({ century: century, people: people });
         } else {
-            centuries[known].ageSum += ageSum;
-            centuries[known].count++;
+            centuries[known].people.push(item);
         }
     }
+    return centuries;
+}
+
+console.log(groupBy(obj, a => Math.ceil(a / 100)));
+
+function averageExpectancyByCentury(items) {
     let res = [];
-    for (let century of centuries) {
-        res.push({ century: century.century, averageExpectancy: Math.round(century.ageSum / century.count) });
+    for (let item of items) {
+        let ageSum = 0;
+        for (let person of item.people) {
+            ageSum += person.died - person.born;
+        }
+        res.push({ century: item.century, averageExpectancy: Math.round(ageSum / item.people.length) });
+        console.log(item.people.length);
     }
     return res;
 }
 
-lifeExpectancyByCentury(obj);
+console.log(averageExpectancyByCentury(groupBy(obj, a => Math.ceil(a / 100))));
