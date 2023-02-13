f43253239f632defcf4f74dd08d8817e7f20035e
Yana <yana.senko25@gmail.com>
2023-02-13 14:49:04 +0100

    changed to prototypes

--- src/task13/task13.js
+++ src/task13/task13.js
@@ -1,22 +1,25 @@
-class Vec {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    get length() {
-        return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
-    }
-    plus(vector) {
-        return new Vec(this.x + vector.x, this.y + vector.y);
-    }
-    minus(vector) {
-        return new Vec(this.x - vector.x, this.y - vector.y);
-    }
+function Vec(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vec.prototype.length = function () {
+    return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+};
+Vec.prototype.plus = function (vector) {
+    return new Vec(this.x + vector.x, this.y + vector.y);
+}
+
+Vec.prototype.minus = function (vector) {
+    return new Vec(this.x - vector.x, this.y - vector.y);
 }
 
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
-console.log(new Vec(3, 4).length);
-// → 5
+console.log(new Vec(12, 5));
+// 13
+
+
+
