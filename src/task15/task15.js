abc359a744070bda88675318d465770d7bdaf5fe
Yana <yana.senko25@gmail.com>
2023-02-13 17:35:02 +0100

    task15 init

--- src/task15/task15.js
+++ src/task15/task15.js
@@ -3,12 +3,6 @@ class ArraySeq {
         this.x = -1;
         this.content = array;
     }
-    get(x) {
-        return this.content[x];
-    }
-    set(x, value) {
-        this.content[x] = value;
-    }
     next() {
         if (this.x == this.content.length - 1) return true;
         this.x++;
@@ -22,28 +16,26 @@ class ArraySeq {
 function logFive(sequence) {
     for (var i = 0; i < 5; i++) {
         if (!sequence.next())
-            //  console.log(sequence.get(i));
             console.log(sequence.current());
     }
 }
 logFive(new ArraySeq([1, 2, 6, 8]));
 
 
-
-function RangeSeq(from, to) {
-    this.x = from - 1;
-    this.to = to;
-}
-RangeSeq.prototype.next = function () {
-    if (this.x >= this.to)
+class RangeSeq {
+    constructor(from, to) {
+        this.x = from - 1;
+        this.to = to;
+    };
+    next() {
+        if (this.x >= this.to)
+            return true;
+        this.x++;
         return false;
-    this.x++;
-    return true;
-};
-RangeSeq.prototype.current = function () {
-    return this.x;
-};
-
+    };
+    current() {
+        return this.x;
+    };
+}
 
 logFive(new RangeSeq(100, 1000));
-// → 100
