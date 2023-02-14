9b716949351174a92f2a60483cba5aa2506091f6
Yana <yana.senko25@gmail.com>
2023-02-14 09:50:20 +0100

    Vec to Vector

--- src/task13/task13.js
+++ src/task13/task13.js
@@ -1,24 +1,23 @@
-function Vec(x, y) {
+function Vector(x, y) {
     this.x = x;
     this.y = y;
 }
 
-Vec.prototype.length = function () {
+Vector.prototype.length = function () {
     return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
 };
-Vec.prototype.plus = function (vector) {
-    return new Vec(this.x + vector.x, this.y + vector.y);
+Vector.prototype.plus = function (vector) {
+    return new Vector(this.x + vector.x, this.y + vector.y);
+}
+Vector.prototype.minus = function (vector) {
+    return new Vector(this.x - vector.x, this.y - vector.y);
 }
 
-Vec.prototype.minus = function (vector) {
-    return new Vec(this.x - vector.x, this.y - vector.y);
-}
-
-console.log(new Vec(1, 2).plus(new Vec(2, 3)));
-// → Vec{x: 3, y: 5}
-console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// → Vec{x: -1, y: -1}
-console.log(new Vec(12, 5));
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(12, 5));
 // 13
 
 
