f9ce92d071d5110bd17df63d65b9c6451e459842
Yana <yana.senko25@gmail.com>
2023-02-15 10:37:11 +0100

    made vector a class and length a method

--- src/task13/task13.js
+++ src/task13/task13.js
@@ -1,11 +1,14 @@
-function Vector(x, y) {
-    this.x = x;
-    this.y = y;
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    length() {
+        return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+    };
 }
 
-Vector.prototype.length = function () {
-    return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
-};
 Vector.prototype.plus = function (vector) {
     return new Vector(this.x + vector.x, this.y + vector.y);
 }
