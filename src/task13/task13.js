b51373b75fc290233d1b48759b4f2d2d72b3c947
Yana <yana.senko25@gmail.com>
2023-03-06 11:52:09 +0100

    Object.defineProperty, removed class

--- src/task13/task13.js
+++ src/task13/task13.js
@@ -1,13 +1,11 @@
-class Vector {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    length() {
-        return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
-    };
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
 }
+
+Object.defineProperty(Vector.prototype, "length", {
+    get: function () { return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)); }
+});
 
 Vector.prototype.plus = function (vector) {
     return new Vector(this.x + vector.x, this.y + vector.y);
@@ -20,8 +18,7 @@ console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
-console.log(new Vector(12, 5));
+console.log(new Vector(12, 5).length);
 // 13
-
 
 
