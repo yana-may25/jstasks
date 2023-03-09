17f59db2fe1531acd4948b780ddff777c438eae5
Yana <yana.senko25@gmail.com>
2023-03-09 11:09:41 +0100

    using toporny method

--- src/task22/task22.js
+++ src/task22/task22.js
@@ -1,4 +1,13 @@
-import { MOUNTAINS } from 'ext\mountains.js'
+// import { MOUNTAINS } from '../../ext/mountains';
+var MOUNTAINS = [
+    { name: "Kilimanjaro", height: 5895, country: "Tanzania" },
+    { name: "Everest", height: 8848, country: "Nepal" },
+    { name: "Mount Fuji", height: 3776, country: "Japan" },
+    { name: "Mont Blanc", height: 4808, country: "Italy/France" },
+    { name: "Vaalserberg", height: 323, country: "Netherlands" },
+    { name: "Denali", height: 6168, country: "United States" },
+    { name: "Popocatepetl", height: 5465, country: "Mexico" }
+];
 
 export function addTable() {
     let array = MOUNTAINS;
@@ -23,4 +32,4 @@ export function addTable() {
         }
     }
     myTableDiv.appendChild(table);
-}
+};
