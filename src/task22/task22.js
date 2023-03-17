45ab8e4d5caa3c5362fa7fed145bf28cd1201722
Yana <yana.senko25@gmail.com>
2023-03-17 12:22:32 +0100

    super-puper

--- src/task22/task22.js
+++ src/task22/task22.js
@@ -1,16 +1,4 @@
-// import { MOUNTAINS } from '../../ext/mountains';
-var MOUNTAINS = [
-    { name: "Kilimanjaro", height: 5895, country: "Tanzania" },
-    { name: "Everest", height: 8848, country: "Nepal" },
-    { name: "Mount Fuji", height: 3776, country: "Japan" },
-    { name: "Mont Blanc", height: 4808, country: "Italy/France" },
-    { name: "Vaalserberg", height: 323, country: "Netherlands" },
-    { name: "Denali", height: 6168, country: "United States" },
-    { name: "Popocatepetl", height: 5465, country: "Mexico" }
-];
-
-export function addTable() {
-    let array = MOUNTAINS;
+function addTable(array) {
     var myTableDiv = document.getElementById("mountains");
     var table = document.createElement('table');
     var tr = document.createElement('tr');
