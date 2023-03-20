b08718a3fd49da3ce5b7dff5d8b5d70c6bb1f44e
Yana <yana.senko25@gmail.com>
2023-03-20 16:08:07 +0100

    removed classes

--- src/task15/task15.js
+++ src/task15/task15.js
@@ -1,19 +1,17 @@
-class ArraySeq {
-    constructor(array) {
-        this.x = -1;
-        this.content = array;
+function ArraySeq(array) {
+    this.x = -1;
+    this.content = array;
+}
+ArraySeq.prototype.next = function () {
+    if (this.x == this.content.length - 1) {
+        return true;
+    } else {
+        this.x++;
     }
-    next() {
-        if (this.x == this.content.length - 1) {
-            return true;
-        } else {
-            this.x++;
-        }
-        return false;
-    }
-    current() {
-        return this.content[this.x];
-    }
+    return false;
+}
+ArraySeq.prototype.current = function () {
+    return this.content[this.x];
 }
 
 function logFive(sequence) {
@@ -26,22 +24,20 @@ function logFive(sequence) {
 
 logFive(new ArraySeq([1, 2, 6, 8]));
 
-class RangeSeq {
-    constructor(from, to) {
-        this.x = from - 1;
-        this.to = to;
+function RangeSeq(from, to) {
+    this.x = from - 1;
+    this.to = to;
+}
+RangeSeq.prototype.next = function () {
+    if (this.x >= this.to) {
+        return true;
+    } else {
+        this.x++;
     }
-    next() {
-        if (this.x >= this.to) {
-            return true;
-        } else {
-            this.x++;
-        }
-        return false;
-    }
-    current() {
-        return this.x;
-    }
+    return false;
+}
+RangeSeq.prototype.current = function () {
+    return this.x;
 }
 
 logFive(new RangeSeq(100, 1000));
