14b5d8590f34390ff4f4e1daa3092b1e5a4aacb3
yana-may25 <75950794+yana-may25@users.noreply.github.com>
2023-03-29 14:41:16 +0200

    Task15 (#16)
    * .
    
    * task15 init
    
    * wtf
    
    * {}
    
    * removed classes
    
    * removed classes
    
    ---------
    
    Co-authored-by: Yana <yana.senko25@gmail.com>

---
+++ src/task15/task15.js
@@ -0,0 +1,45 @@
+function ArraySeq(array) {
+    this.x = -1;
+    this.content = array;
+}
+
+ArraySeq.prototype.next = function () {
+    if (this.x == this.content.length - 1) {
+        return true;
+    } else {
+        this.x++;
+    }
+    return false;
+}
+
+ArraySeq.prototype.current = function () {
+    return this.content[this.x];
+}
+
+function logFive(sequence) {
+    for (var i = 0; i < 5; i++) {
+        if (!sequence.next()) {
+            console.log(sequence.current());
+        }
+    }
+}
+
+logFive(new ArraySeq([1, 2, 6, 8]));
+
+function RangeSeq(from, to) {
+    this.x = from - 1;
+    this.to = to;
+}
+RangeSeq.prototype.next = function () {
+    if (this.x >= this.to) {
+        return true;
+    } else {
+        this.x++;
+    }
+    return false;
+}
+RangeSeq.prototype.current = function () {
+    return this.x;
+}
+
+logFive(new RangeSeq(100, 1000));
